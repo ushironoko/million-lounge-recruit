8d3230013d3fdee06e6bed5e06b0f80aac6c4880
ushironoko <apple19940820@yahoo.co.jp>
2019-03-03 16:24:01 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'universal',
+  srcDir: 'app/',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'ミリシタラウンジ求人',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    '@nuxtjs/pwa',
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  }
+}
